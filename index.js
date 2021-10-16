4e4f2075ec6abb28b7f8855b44897b76b8cf8acf
Suraj Kumar <vermasuraj412@gmail.com>
2021-10-16 17:31:43 +0530

    keys are working

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
 /*
-    we are not calling handleClick()
-    since function will get called while adding the function in eventListener
-    which we don't want, we want to call the function when click happens
-
-    so
-    document.querySelector("button").addEventListener("click", handleClick()); will call the function while loading
-    and
-    document.querySelector("button").addEventListener("click", handleClick); will call the function on clicking the button
-
-    also we can also use anonymous function since this will only be defined inside an eventListener
+    instead of playing audio using index, we can also use 'this' keyword to access object and hence innerHTML
+    as this.innerHTML which will be one of these ['w', 'a', 's', 'd', 'j', 'k', 'l']
+    and we can use these to map audio to buttons
+    
+    setTimeout funtion takes a callBack functio and a time in ms
 */
 
 let drumButtons = document.querySelectorAll(".drum");
 
 for(let i = 0; i<drumButtons.length; i++) {
     drumButtons[i].addEventListener("click", function() {
-        let dir = "res/audio/";
-        switch(i) {
-            case 3:
-                dir += "tom-1.mp3";
-                break;
-            case 4:
-                dir += "tom-2.mp3";
-                break;
-            case 5:
-                dir += "tom-3.mp3";
-                break;
-            case 6:
-                dir += "tom-4.mp3";
-                break;
-            case 1:
-                dir += "kick-bass.mp3";
-                break;
-            case 0:
-                dir += "crash.mp3";
-                break;
-            case 2:
-                dir += "snare.mp3";
-                break;
-        }
-        let currAudio = new Audio(dir)
-        currAudio.play();
+        let buttonKey = this.innerHTML;
+        makeSound(buttonKey);
+        buttonAnimation(buttonKey);
     });
+}
+
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    let dir = "res/audio/";
+    switch(key) {
+        case 'w':
+            dir += "crash.mp3";
+            break;
+        case 'a':
+            dir += "kick-bass.mp3";
+            break;
+        case 's':
+            dir += "snare.mp3";
+            break;
+        case 'd':
+            dir += "tom-1.mp3";
+            break;
+        case 'j':
+            dir += "tom-2.mp3";
+            break;
+        case 'k':
+            dir += "tom-3.mp3";
+            break;
+        case 'l':
+            dir += "tom-4.mp3";
+            break;
+        default:
+            console.log("no button found")
+            break;
+    }
+    let currAudio = new Audio(dir)
+    currAudio.play();
+}
+
+function buttonAnimation(key) {
+    let currButton = document.querySelector("."+key);
+    currButton.classList.add("pressed");
+    setTimeout(function() {
+        currButton.classList.remove("pressed");
+    }, 50);
 }
